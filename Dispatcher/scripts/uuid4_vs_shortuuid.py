48d14df3eb8ff73ab0fe683294a5dc48265b71d4
boisde <boisde1117@gmail.com>
2017-01-09 15:34:30 +0800

    uuid vs shortuuid collision comparison

--- Dispatcher/scripts/uuid4_vs_shortuuid.py
+++ Dispatcher/scripts/uuid4_vs_shortuuid.py
@@ -1,16 +1,10 @@
-# import shortuuid
-# import uuid
-#
-# s1, s2 = set(), set()
-# for i in range(200000):
-#     s1.add(shortuuid.ShortUUID().random(length=6))
-#     s2.add(str(uuid.uuid4().fields[-1])[:6])
-# print(("shortuuid: ", len(s1)))
-# print(("uuid4: ", len(s2)))
+import shortuuid
+import uuid
 
-import os
+s1, s2 = set(), set()
+for i in range(200000):
+    s1.add(shortuuid.ShortUUID().random(length=6))
+    s2.add(str(uuid.uuid4().fields[-1])[:6])
+print(("shortuuid: ", len(s1)))
+print(("uuid4: ", len(s2)))
 
-for e in os.scandir(os.curdir):
-    if e.name[-4:] == '.bak':
-        print(e.path)
-        os.remove(e.path)
